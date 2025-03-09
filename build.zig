0dcb2fbc0de24562b092230012e39d93c1c32757
milogreg <oldesoot@gmail.com>
2025-03-09 10:08:21 -0700

    Update build.zig and build.zig.zon

--- build.zig
+++ build.zig
@@ -5,8 +5,7 @@ pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
     const target = b.standardTargetOptions(.{});
 
-    const exe = b.addExecutable(.{
-        .name = "example",
+    const exe_mod = b.createModule(.{
         .root_source_file = b.path("src/main.zig"),
         .target = b.resolveTargetQuery(.{
             .cpu_arch = .wasm32,
@@ -27,8 +26,12 @@ pub fn build(b: *std.Build) void {
                 // .tail_call,
             }),
         }),
-
         .optimize = optimize,
+    });
+
+    const exe = b.addExecutable(.{
+        .name = "example",
+        .root_module = exe_mod,
     });
 
     exe.entry = .disabled;
@@ -52,28 +55,7 @@ pub fn build(b: *std.Build) void {
 
     const check_exe = b.addExecutable(.{
         .name = "example",
-        .root_source_file = b.path("src/main.zig"),
-        .target = b.resolveTargetQuery(.{
-            .cpu_arch = .wasm32,
-            .os_tag = .freestanding,
-            .cpu_features_add = std.Target.wasm.featureSet(&[_]std.Target.wasm.Feature{
-                // .atomics,
-                .bulk_memory,
-                .exception_handling,
-                .extended_const,
-                // .multimemory,
-                .multivalue,
-                .mutable_globals,
-                .nontrapping_fptoint,
-                .reference_types,
-                .relaxed_simd,
-                .sign_ext,
-                .simd128,
-                // .tail_call,
-            }),
-        }),
-
-        .optimize = optimize,
+        .root_module = exe_mod,
     });
 
     check_exe.entry = .disabled;
@@ -81,7 +63,7 @@ pub fn build(b: *std.Build) void {
 
     check_exe.stack_size = std.wasm.page_size * 2;
     check_exe.initial_memory = std.wasm.page_size * 4;
-    check_exe.max_memory = std.wasm.page_size * 1024 * 64;
+    check_exe.max_memory = std.wasm.page_size * (1 << 16);
 
     const check_step = b.step("check", "check compilation");
     check_step.dependOn(&check_exe.step);
